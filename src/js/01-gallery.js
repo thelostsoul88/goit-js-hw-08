d14c74759ea60f7e22dc41a8060dc19698a387da
Dmitriy <48189332+thelostsoul88@users.noreply.github.com>
2023-01-27 13:56:20 +0200

    first task complete

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+// доп стили
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galeryImg = document.querySelector('.gallery');
+const imageItemsMarkup = createEl(galleryItems);
+
+function createEl(el) {
+  return el
+    .map(
+      ({
+        preview,
+        original,
+        description,
+      }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    )
+    .join('');
+}
+galeryImg.insertAdjacentHTML('beforeend', imageItemsMarkup);
+galeryImg.addEventListener('click', e => {
+  e.preventDefault();
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  scrollZoom: false,
+});
