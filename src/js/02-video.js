14e968bdb2057518a9e268503c58201058963140
Dmitriy <48189332+thelostsoul88@users.noreply.github.com>
2023-01-28 21:43:21 +0200

    second task complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function setTime({ seconds }) {
+  try {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  } catch (error) {
+    console.error('Set err', error.message);
+  }
+}
+
+player.on('timeupdate', throttle(setTime, 1000));
+
+function getTime() {
+  try {
+    return localStorage.getItem('videoplayer-current-time') || 0;
+  } catch (error) {
+    console.error('Get err', error.message);
+  }
+}
+
+player.setCurrentTime(getTime());
