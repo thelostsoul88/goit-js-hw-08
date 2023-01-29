482c3e5f63c7f3f3e69691d6573775ca29e3c67d
Dmitriy <48189332+thelostsoul88@users.noreply.github.com>
2023-01-29 19:55:44 +0200

    third task complete

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+const KEY = 'feedback-form-state';
+
+form.addEventListener('submit', onSubmit);
+form.addEventListener('input', throttle(setLocalStorage, 500));
+window.addEventListener('DOMContentLoaded', getLocalSorage);
+
+let values = {
+  email,
+  message,
+};
+
+function setLocalStorage(e) {
+  try {
+    values.email = email.value;
+    values.message = message.value;
+    localStorage.setItem(KEY, JSON.stringify(values));
+  } catch (error) {
+    console.error('Set err', error.message);
+  }
+}
+
+function getLocalSorage() {
+  try {
+    const getStorege = localStorage.getItem(KEY);
+    const getParce = JSON.parse(getStorege);
+    if (getParce) {
+      email.value = getParce.email;
+      message.value = getParce.message;
+    }
+  } catch (error) {
+    console.error('Get err', error.message);
+  }
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  if (email.value === '' || message.value === '') {
+    alert('Heyup! form must be completed');
+    return;
+  }
+  console.log(`  Email: ${email.value}, 
+  Message: ${message.value}`);
+  e.currentTarget.reset();
+  localStorage.removeItem(KEY);
+}
